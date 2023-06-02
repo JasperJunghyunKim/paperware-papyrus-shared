563b74c8f435661618f224ea2102469f2987022b
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-06-02 11:46:03 +0900

    재고그룹 모델 수정

--- models/stock-group.ts
+++ models/stock-group.ts
@@ -1,1 +1,39 @@
-export default interface StockGroup {}
+import Company from "./company";
+import Packaging from "./packaging";
+import PaperCert from "./paper-cert";
+import PaperColor from "./paper-color";
+import PaperColorGroup from "./paper-color-group";
+import PaperPattern from "./paper-pattern";
+import Plan from "./plan";
+import Product from "./product";
+import Warehouse from "./warehouse";
+
+type WarehouseWithoutCompany = Omit<Warehouse, 'company'>
+interface StockGroupPlan {
+    id: number;
+    planNo: string;
+    orderStock: {
+        order: {
+            id: number;
+            orderNo: string;
+            wantedDate: string;
+            partnerCompany: Company;
+        }
+    } | null;
+}
+
+export default interface StockGroup {
+    warehouse: WarehouseWithoutCompany | null;
+    product: Product;
+    packaging: Packaging;
+    grammage: number;
+    sizeX: number;
+    sizeY: number;
+    paperColorGroup: PaperColorGroup | null;
+    paperColor: PaperColor | null;
+    paperPattern: PaperPattern | null;
+    paperCert: PaperCert | null;
+    plan: StockGroupPlan | null;
+    totalQuantity: number;
+    availableQuantity: number;
+}
