edf3772ea21f25d82d7d7ab184d2ed8af883e28f
권진오 <kjo1333@gmail.com>
2023-06-12 10:43:00 +0900

    card 모델 수정

--- api/accounted/by-card.request.ts
+++ api/accounted/by-card.request.ts
@@ -1,4 +1,4 @@
 import { ByCard } from '../../models';
 
-export type ByCardCreateRequest = Omit<ByCard, 'accountedId' | 'partnerNickName' | 'cardName' | 'cardNumber' | 'cardCompany' | 'accountName'>;
+export type ByCardCreateRequest = Omit<ByCard, 'bankAccountId' | 'accountedId' | 'partnerNickName' | 'cardName' | 'cardNumber' | 'cardCompany' | 'accountName'>;
 export type ByCardUpdateRequest = Omit<ByCard, 'companyId' | 'accountedId' | 'companyRegistrationNumber' | 'partnerNickName' | 'accountedId' | 'cardId' | 'cardName' | 'cardNumber' | 'cardCompany' | 'bankAccountId' | 'accountName'>;
--- api/accounted/by-card.response.ts
+++ api/accounted/by-card.response.ts
@@ -1,4 +1,4 @@
 import { ByCard } from '../../models';
 
 export type ByCardQuery = ByCard;
-export type ByCardItemResponse = ByCard;
+export type ByCardItemResponse = Omit<ByCard, 'bankAccountId' | 'accountName'>;
