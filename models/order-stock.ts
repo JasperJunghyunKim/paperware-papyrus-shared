62bb1f245743ac75072b70dfbc67937129e94c0c
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-06-29 13:39:08 +0900

    orderstock 모델 수정

--- models/order-stock.ts
+++ models/order-stock.ts
@@ -1,4 +1,4 @@
-import { Enum, Location } from '.';
+import { Company, Enum, Location, Packaging, PaperCert, PaperColor, PaperColorGroup, PaperPattern, Product, Warehouse } from '.';
 import { Model } from '..';
 
 export default interface OrderStock {
@@ -15,4 +15,24 @@ export default interface OrderStock {
     assignStockEvent: Model.StockEvent;
     companyId: number;
   }[];
+  // 정상거래의 주문 원지 정보
+  company: Company;
+  planId: number | null;
+  warehouse: {
+    id: number;
+    name: string;
+    code: string;
+    isPublic: boolean;
+    address: string;
+  } | null;
+  product: Product;
+  packaging: Packaging;
+  grammage: number;
+  sizeX: number;
+  sizeY: number;
+  paperColorGroup: PaperColorGroup | null;
+  paperColor: PaperColor | null;
+  paperPattern: PaperPattern | null;
+  paperCert: PaperCert | null;
+  quantity: number;
 }
