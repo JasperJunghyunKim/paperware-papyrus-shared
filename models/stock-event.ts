7acfbb9bf1ca8f0fc43690a3e2eb6185ba1608a5
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-07-07 18:12:47 +0900

    모델수정

--- models/stock-event.ts
+++ models/stock-event.ts
@@ -1,5 +1,32 @@
-import { StockEventStatus } from './enum';
+import Company from './company';
+import {
+  DiscountType,
+  OfficialPriceType,
+  PlanType,
+  PriceUnit,
+  StockEventStatus,
+} from './enum';
 import Stock from './stock';
+
+interface Order {
+  srcCompany: Company;
+  dstCompany: Company;
+}
+
+interface OrderStock {
+  order: Order;
+}
+
+interface OrderProcess {
+  order: Order;
+}
+
+interface Plan {
+  planNo: string;
+  type: PlanType;
+  orderStock: OrderStock | null;
+  orderProcess: OrderProcess | null;
+}
 
 export default interface StockEvent {
   id: number;
@@ -7,4 +34,5 @@ export default interface StockEvent {
   change: number;
   status: StockEventStatus;
   createdAt: string;
+  plan: Plan;
 }
