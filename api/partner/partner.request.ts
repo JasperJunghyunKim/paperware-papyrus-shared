66420bc3e1f0dda92f69541ee2113be27439ad18
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-07-08 10:28:31 +0900

    모델추가

--- api/partner/partner.request.ts
+++ api/partner/partner.request.ts
@@ -1,3 +1,20 @@
 import { PaginationQuery } from '../../models/pagination';
 
 export type PartnerQuery = PaginationQuery;
+
+/** 세금계산서 담당자 추가 */
+export type PartnerTaxManagerCreateRequest = {
+  companyRegistrationNumber: string;
+  name: string;
+  phoneNo: string;
+  email: string;
+  isDefault: boolean;
+};
+
+/** 세금계산서 담당자 수정 */
+export type PartnerTaxManagerUpdateRequest = {
+  name: string;
+  phoneNo: string;
+  email: string;
+  isDefault: boolean;
+};
