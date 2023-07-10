2ec7adcf28ceea166f5183fb91e7f2fdea023a56
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-07-10 15:49:14 +0900

    모델수정

--- api/trade/order.request.ts
+++ api/trade/order.request.ts
@@ -101,6 +101,7 @@ export interface OrderDepositTradePriceUpdateRequest {
 export interface TradePriceUpdateRequest {
   suppliedPrice: number;
   vatPrice: number;
+  isSyncPrice: boolean;
   orderStockTradePrice?: OrderStockTradePriceUpdateRequest;
   orderDepositTradePrice?: OrderStockTradePriceUpdateRequest;
 }
