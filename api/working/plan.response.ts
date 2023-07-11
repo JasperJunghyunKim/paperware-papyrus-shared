28e784b3109d3ae11123f21c37e680eb1c0e08f3
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-07-11 16:52:07 +0900

    실투입재고 상세 모델

--- api/working/plan.response.ts
+++ api/working/plan.response.ts
@@ -4,3 +4,10 @@ import { PaginationResponse } from '../../models/pagination';
 export type PlanListResponse = PaginationResponse<Plan>;
 export type PlanItemResponse = Plan;
 export type PlanInputListResponse = PaginationResponse<StockEvent>;
+
+/** 실투입 재고 상세 */
+export interface InputStockResponse {
+  stockId: number;
+  quantity: number;
+  useRemainder: boolean;
+}
