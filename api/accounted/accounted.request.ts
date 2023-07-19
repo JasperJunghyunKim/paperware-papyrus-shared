250c08332c341d13ae5d7ed903a6c2c0615e96e8
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-07-19 18:38:28 +0900

    모듈추가

--- api/accounted/accounted.request.ts
+++ api/accounted/accounted.request.ts
@@ -1,5 +1,6 @@
 import { PaginationQuery } from '../../models/pagination';
 import { Accounted } from '../../models';
+import { AccountedType } from '../../models/enum';
 
 export type AccountedQuery = Pick<
   Accounted,
@@ -9,3 +10,11 @@ export type AccountedQuery = Pick<
   | 'accountedMethod'
   | 'accountedType'
 > & { accountedFromDate: string; accountedToDate: string } & PaginationQuery;
+
+export interface AccountedUnpaidListQuery extends PaginationQuery {
+  accountedType: AccountedType;
+  // 검색 필드
+  companyRegistrationNumbers?: string;
+  minAmount?: number;
+  maxAmount?: number;
+}
