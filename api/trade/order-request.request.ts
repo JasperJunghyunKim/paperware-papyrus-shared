bced7e3ddb827dda167546816ffaab6a05c09e9b
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-07-19 10:46:06 +0900

    모델추가

--- api/trade/order-request.request.ts
+++ api/trade/order-request.request.ts
@@ -1,1 +1,20 @@
-export interface OrderRequestCreateRequest {}
+import { PaginationQuery } from 'src/@shared/models/pagination';
+
+/** 퀵주문 목록 */
+export interface OrderRequestListQuery extends PaginationQuery {
+  srcCompanyId: number;
+  dstCompanyId: number;
+}
+
+/** 퀵주문 생성 */
+export interface OrderRequestCreateRequest {
+  dstCompanyId: number;
+  locationId?: number;
+  wantedDate?: string;
+  memo?: string;
+  orderRequestItems: {
+    item: string;
+    quantity?: string;
+    memo?: string;
+  }[];
+}
