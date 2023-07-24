a9244bbb60be4c4c610d1695f5d22c7c18576bdf
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-07-24 16:03:44 +0900

    미수금목록

--- api/accounted/accounted.response.ts
+++ api/accounted/accounted.response.ts
@@ -3,3 +3,29 @@ import { PaginationResponse } from '../../models/pagination';
 
 export type AccountedListResponse = PaginationResponse<Accounted>;
 export type AccountedItemResponse = Accounted;
+
+export type AccountedUnpaidListResponse = PaginationResponse<{
+  companyRegistrationNumber: string;
+  partnerNickName: string;
+  creditLimit: number;
+  totalPrice: number;
+  price1: number; // N+1월
+  price2: number; // N월
+  price3: number; // N-1월
+  price4: number; // N-2월
+  price5: number; // N-3월
+  price6: number; // N-4월
+  price7: number; // N-5월
+}> & {
+  // footer
+  totalPrice: {
+    totalPrice: number;
+    price1: number; // N+1월
+    price2: number; // N월
+    price3: number; // N-1월
+    price4: number; // N-2월
+    price5: number; // N-3월
+    price6: number; // N-4월
+    price7: number; // N-5월
+  };
+};
