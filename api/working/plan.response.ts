4d55197a4cf3f2f23e9e90e236a8e0e73f6cff73
hyejun <jollyer@outlook.kr>
2023-07-26 13:48:34 +0900

    .

--- api/working/plan.response.ts
+++ api/working/plan.response.ts
@@ -1,7 +1,8 @@
-import { Plan, Stock, StockEvent } from '../../models';
+import { Model } from '../..';
+import { Company, Enum, Order, Plan, Stock, StockEvent } from '../../models';
 import { PaginationResponse } from '../../models/pagination';
 
-export type PlanListResponse = PaginationResponse<Plan>;
+export type PlanListResponse = PaginationResponse<PlanListItem>;
 export type PlanItemResponse = Plan;
 export type PlanInputListResponse = PaginationResponse<StockEvent>;
 
@@ -11,4 +12,31 @@ export interface InputStockResponse {
   quantity: number;
   useRemainder: boolean;
   stock: Stock;
+}
+
+export interface PlanListItem {
+  id: number;
+  planNo: string;
+  status: Enum.PlanStatus;
+  type: Enum.PlanType;
+  company: Company;
+  createdAt: string;
+  assignStockEvent: StockEvent | null;
+  targetStockEvent: StockEvent[];
+  orderStock: {
+    dstLocation: Model.Location;
+    wantedDate: string;
+    order: Order;
+  } | null;
+  orderProcess: {
+    dstLocation: Model.Location;
+    dstWantedDate: string;
+    srcLocation: Model.Location;
+    srcWantedDate: string;
+    order: Order;
+  } | null;
+  task: {
+    status: Enum.TaskStatus;
+    type: Enum.TaskType;
+  }[];
 }
