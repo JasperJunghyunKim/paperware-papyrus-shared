d87a9a8fcc7c2a3d15a39242723213c9f903e4e9
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-08-01 17:36:38 +0900

    회사정보 수정

---
+++ api/setting/company.request.ts
@@ -0,0 +1,9 @@
+export interface SettingCompanyUpdateRequest {
+  businessName: string;
+  representative: string;
+  phoneNo: string;
+  faxNo: string;
+  address: string;
+  bizType: string;
+  bizItem: string;
+}
