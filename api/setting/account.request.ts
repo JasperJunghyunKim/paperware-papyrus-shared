b30ade673aabcdac6363055751287ab0e8c5a639
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-08-02 10:53:42 +0900

    최초로그인 정보수정

--- api/setting/account.request.ts
+++ api/setting/account.request.ts
@@ -12,3 +12,10 @@ export interface AccountPhoneNoUpdateRequest {
   phoneNo: string;
   authKey: string;
 }
+
+/** 최초로그인시에만 가능 */
+export interface AccountPasswordAndPhoneNoUpdateRequest {
+  password: string;
+  phoneNo: string;
+  authKey: string;
+}
