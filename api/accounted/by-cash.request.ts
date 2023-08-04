8d3f1040ebcfde0a52e4437de4ab7c004b6969eb
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-08-04 14:44:33 +0900

    현금지급 등록/수정

--- api/accounted/by-cash.request.ts
+++ api/accounted/by-cash.request.ts
@@ -2,9 +2,18 @@ import { ByCash } from '../../models';
 
 export type ByCashCreateRequest = Omit<
   ByCash,
-  'accountedId' | 'partnerNickName'
+  | 'accountedId'
+  | 'partnerNickName'
+  | 'companyId'
+  | 'accountedType'
+  | 'accountedMethod'
 >;
 export type ByCashUpdateRequest = Omit<
   ByCash,
-  'companyId' | 'accountedId' | 'companyRegistrationNumber' | 'partnerNickName'
+  | 'companyId'
+  | 'accountedId'
+  | 'companyRegistrationNumber'
+  | 'partnerNickName'
+  | 'accountedType'
+  | 'accountedMethod'
 >;
