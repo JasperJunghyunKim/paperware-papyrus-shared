9458b7ea096bdb53fb374e31f186821a787c203a
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-08-04 10:30:44 +0900

    배송 목록 수정

--- models/shipping-item.ts
+++ models/shipping-item.ts
@@ -1,12 +1,26 @@
 import { Company } from '.';
-import { InvoiceStatus, ShippingStatus } from './enum';
+import { InvoiceStatus, ShippingStatus, ShippingType } from './enum';
 
 export default interface ShippingItem {
   id: number;
+  type: ShippingType;
   shippingNo: string;
   status: ShippingStatus;
   company: Company;
   invoiceCount: number;
+  invoiceWeight: number;
+  manager: {
+    id: number;
+    name: string;
+  } | null;
+  companyRegistrationNumber: string | null;
+  price: number;
+  memo: string;
+  createdAt: string;
+  createdByUser: {
+    id: number;
+    name: string;
+  };
   invoice: {
     invoiceStatus: InvoiceStatus;
   }[];
