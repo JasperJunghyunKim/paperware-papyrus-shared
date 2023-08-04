b48f4d698c294f344f76bc3d5716ea8c303acffd
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-08-04 15:45:52 +0900

    기타 등록/수정

--- api/accounted/by-etc.request.ts
+++ api/accounted/by-etc.request.ts
@@ -1,7 +1,19 @@
 import { ByEtc } from '../../models';
 
-export type ByEtcCreateRequest = Omit<ByEtc, 'accountedId' | 'partnerNickName'>;
+export type ByEtcCreateRequest = Omit<
+  ByEtc,
+  | 'accountedId'
+  | 'partnerNickName'
+  | 'companyId'
+  | 'accountedType'
+  | 'accountedMethod'
+>;
 export type ByEtcUpdateRequest = Omit<
   ByEtc,
-  'companyId' | 'accountedId' | 'companyRegistrationNumber' | 'partnerNickName'
+  | 'companyId'
+  | 'accountedId'
+  | 'companyRegistrationNumber'
+  | 'partnerNickName'
+  | 'accountedType'
+  | 'accountedMethod'
 >;
