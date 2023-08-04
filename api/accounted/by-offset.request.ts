668675c3a19743bc9c0386606c8c76f131c22dc6
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-08-04 15:55:47 +0900

    상계 등록/수정

--- api/accounted/by-offset.request.ts
+++ api/accounted/by-offset.request.ts
@@ -2,9 +2,18 @@ import { ByOffset } from '../../models';
 
 export type ByOffsetCreateRequest = Omit<
   ByOffset,
-  'accountedId' | 'partnerNickName'
+  | 'accountedId'
+  | 'partnerNickName'
+  | 'companyId'
+  | 'accountedType'
+  | 'accountedMethod'
 >;
 export type ByOffsetUpdateRequest = Omit<
   ByOffset,
-  'companyId' | 'accountedId' | 'companyRegistrationNumber' | 'partnerNickName'
+  | 'companyId'
+  | 'accountedId'
+  | 'companyRegistrationNumber'
+  | 'partnerNickName'
+  | 'accountedType'
+  | 'accountedMethod'
 >;
