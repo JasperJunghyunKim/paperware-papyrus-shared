17a0011a68ee66417c5d3069b755107fade6a7d4
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-08-07 16:39:25 +0900

    회계등록(계좌)

--- api/accounted/accounted.request.ts
+++ api/accounted/accounted.request.ts
@@ -1,16 +1,23 @@
 import { PaginationQuery } from '../../models/pagination';
-import { Accounted } from '../../models';
-import { AccountedType } from '../../models/enum';
+import { AccountedType, Subject } from '../../models/enum';
 
-export type AccountedQuery = Pick<
-  Accounted,
-  | 'companyId'
-  | 'companyRegistrationNumber'
-  | 'accountedSubject'
-  | 'accountedMethod'
-  | 'accountedType'
-> & { accountedFromDate: string; accountedToDate: string } & PaginationQuery;
+/** 수금/지급 목록 */
+export type AccountedListQuery = PaginationQuery & {};
 
+/** 수금/지급 등록 (계좌이체) */
+export type AccountedByBankAccountCreatedRequest = {
+  accountedType: AccountedType;
+  companyRegistrationNumber: string;
+  accountedDate: string;
+  accountedSubject: Subject;
+  amount: number;
+  memo?: string;
+  bankAccountId: number;
+};
+
+/** 수금/지급 등록 (현금) */
+
+/** 미수금&미지급 목록 */
 export interface AccountedUnpaidListQuery extends PaginationQuery {
   accountedType: AccountedType;
   // 검색 필드
--- models/accounted.ts
+++ models/accounted.ts
@@ -1,52 +1,24 @@
-import { AccountedType, Method, SecurityStatus, Subject } from './enum';
+import ByBankAccount from './by-bank-account';
+import ByCard from './by-card';
+import ByCash from './by-cash';
+import ByEtc from './by-etc';
+import ByOffSet from './by-offset';
+import BySecurity from './by-security';
+import { AccountedType, Method, Subject } from './enum';
 
 export default interface Accounted {
-  /**
-   * 사업자 식별자
-   */
-  companyId: number;
-  /**
-   * 사업자 번호
-   */
+  id: number;
   companyRegistrationNumber: string;
-  /**
-   * 파트너 닉네임
-   */
-  partnerNickName: string;
-  /**
-   * 지급 식별자
-   */
-  accountedId: number;
-  /**
-   * 지급 타입
-   */
   accountedType: AccountedType;
-  /**
-   * 회계 등록일
-   */
   accountedDate: string;
-  /**
-   * 회계 수단
-   */
   accountedMethod: Method;
-  /**
-   * 계정 과목
-   */
   accountedSubject: Subject;
-  /**
-   * 금액
-   */
   amount: number;
-  /**
-   * 메모
-   */
   memo: string;
-  /**
-   * 구분
-   */
-  gubun: string;
-  /**
-   * 유가증권 상태(optional)
-   */
-  securityStatus?: SecurityStatus;
+  byBankAccount: ByBankAccount | null;
+  byCard: ByCard | null;
+  byCash: ByCash | null;
+  bySecurity: BySecurity | null;
+  byOffset: ByOffSet | null;
+  byEtc: ByEtc | null;
 }
