7968cde0972fc76c19276d86dd222ba3e11d92ea
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-08-07 14:56:56 +0900

    계좌 등록/수정

--- api/inhouse/bank-account.request.ts
+++ api/inhouse/bank-account.request.ts
@@ -1,6 +1,19 @@
+import { AccountType, Bank } from 'src/@shared/models/enum';
 import BankAccount from '../../models/bank-account';
 import { PaginationQuery } from '../../models/pagination';
 
 export type BankAccountQuery = PaginationQuery;
-export type BankAccountCreateRequest = Omit<BankAccount, 'accountId'>;
-export type BankAccountUpdateRequest = Pick<BankAccount, 'accountName'>;
+
+/** 계좌 등록 */
+export type BankAccountCreateRequest = {
+  bank: Bank;
+  accountName: string;
+  accountType: AccountType;
+  accountNumber: string;
+  accountHolder: string;
+};
+
+/** 계좌 수정 */
+export type BankAccountUpdateRequest = {
+  accountName: string;
+};
