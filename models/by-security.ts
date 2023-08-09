1279dcd2dadcd007d3a7b8a901f8c1bdb213854c
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-08-09 11:44:52 +0900

    bySecurity 모델 수정

--- models/by-security.ts
+++ models/by-security.ts
@@ -1,5 +1,8 @@
+import { EndorsementType } from './enum';
 import Security from './security';
 
 export default interface BySecurity {
   security: Security;
+  endorsementType: EndorsementType;
+  endorsement: string;
 }
