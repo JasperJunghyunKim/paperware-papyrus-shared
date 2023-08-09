d1767a5412d795613ce87f9760ad1323df7a1ede
hyeongeun-an <hyeongeun.an@paperware.kr>
2023-08-09 09:19:26 +0900

    byCard 모델 수정

--- models/by-card.ts
+++ models/by-card.ts
@@ -3,7 +3,11 @@ import Card from './card';
 import { AccountedType, Bank, CardCompany, Method, Subject } from './enum';
 
 export default interface ByCard {
+  cardAmount: number;
+  vatPrice: number;
   amount: number;
+  isCharge: boolean;
+  approvalNumber: string;
   card: Card | null;
   bankAccount: BankAccount | null;
 }
