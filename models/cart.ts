bdb0c88a81b9c4b28e60bfd9a9d85d86bf3f1d77
yojihan <yojihan.am@gmail.com>
2023-08-22 10:36:40 +0900

    카트 모델 수정

--- models/cart.ts
+++ models/cart.ts
@@ -1,21 +1,35 @@
+import Company from './company';
 import { CardCompany, CartType } from './enum';
+import Packaging from './packaging';
+import PaperCert from './paper-cert';
+import PaperColor from './paper-color';
+import PaperColorGroup from './paper-color-group';
+import PaperPattern from './paper-pattern';
+import Product from './product';
 
 export default interface Cart {
   id: number;
   type: CartType;
   // 원지 정보
-  companyId: number;
+  company: Company;
   planId: number | null;
-  warehouseId: number | null;
-  productId: number;
-  packagingId: number;
+  warehouse: {
+    id: number;
+    name: string;
+    isPublic: boolean;
+    address: string;
+  } | null;
+  product: Product;
+  packaging: Packaging;
   grammage: number;
   sizeX: number;
   sizeY: number;
-  paperColorGroupId: number | null;
-  paperColorId: number | null;
-  paperPatternId: number | null;
-  paperCertId: number | null;
+  paperColorGroup: PaperColorGroup | null;
+  paperColor: PaperColor | null;
+  paperPattern: PaperPattern | null;
+  paperCert: PaperCert | null;
   quantity: number;
   memo: string;
+  totalAvailableQuantity: number;
+  totalQuantity: number;
 }
